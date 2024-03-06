b68c0e8ffbc147b935cf1f6e6ea21c6f6c064697
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-06 17:17:21 +0100

    Feature: Dynamic pixel resizing with a slider

--- script.js
+++ script.js
@@ -72,10 +72,3 @@ const slider = document.getElementById("myRange");
 slider.oninput = function () {
   initializeSketchScreen(sketchContainer, this.value);
 };
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
